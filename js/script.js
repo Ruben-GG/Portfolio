88f7da076e0db7c3cc073c961af78a670ee06072
Ruben <gallardogruben@gmail.com>
2020-02-10 18:36:53 +0100

    Añadiendo portfolio y otras cosas

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+function maquina(container,text,interval){
+    longitud = text.length;
+    cnt = document.querySelector(container);
+    var i=0;
+    timer = setInterval(function(){
+        cnt.innerHTML = cnt.innerHTML.substr(0,cnt.innerHTML.length-1) + text.charAt(i) + "_";
+        if(i >= longitud){
+            clearInterval(timer);
+            return true;
+        } else {
+            i++;
+        }
+        },interval);
+};
+var text = 'Software Developer';
+maquina(".ocupation",text,70);
